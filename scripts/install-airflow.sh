3a8594bb7c7376b1c8a8c33b339db2f4679a75b2
hwalton <harveywalton.hw@gmail.com>
2025-10-05 20:54:01 +0100

    airflow dags from docker

--- scripts/install-airflow.sh
+++ scripts/install-airflow.sh
@@ -12,6 +12,11 @@ kubectl delete deployment airflow-postgresql --ignore-not-found=true
 kubectl delete service airflow-webserver --ignore-not-found=true
 kubectl delete service airflow-postgresql --ignore-not-found=true
 
+
+# Build Airflow Docker image
+eval $(minikube docker-env)
+docker build -f Dockerfile.airflow -t gbr-airflow:latest .
+
 # Deploy Airflow in default namespace
 kubectl apply -f k8s/airflow-rbac.yaml
 kubectl apply -f k8s/airflow-simple.yaml
